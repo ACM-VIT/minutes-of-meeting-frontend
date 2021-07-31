8de087b5da226a6fc424195bc4404239f1292994
ANISH0309 <mittalanish789@gmail.com>
2021-07-31 20:37:09 +0530

    feat: add date to mom card
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 
-function postcard({ title, id }) {
+function postcard({ title, id, createdAt }) {
+  function truncate(str) {
+    return str.length > 15 ? `${str.substring(0, 20)}...` : str;
+  }
+
   return (
     <div className="my-2 mx-8">
       <div className="relative h-64 w-64 bg-white rounded-lg border border-black">
         <div className="flex-col">
-          <div className="text-center my-4 text-2xl">
-            <p>{title}</p>
+          <div className="text-center mt-4 text-2xl">
+            <p>{truncate(title)}</p>
           </div>
+          <div className="text-center">{createdAt}</div>
           <div className="py-8 text-center">
             {/* {console.log(User)}
             <img src={User.image} alt="google" />
