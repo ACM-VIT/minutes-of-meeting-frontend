b85d5887229b8b7ecb5e647fef9bfb660b8305b5
ANISH0309 <mittalanish789@gmail.com>
2021-09-08 15:08:17 +0530

    feat: add urls
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

---
+++ src/urls.jsx
@@ -0,0 +1,6 @@
+export default {
+  CLIENT_BASEURL: "http://localhost:3000",
+  SERVER_BASEURL: "http://localhost:9000",
+  GOOGLE_URL: "http://localhost:9000/auth/google",
+  GOOGLE_LOGOUT: "http://localhost:9000/auth/logout",
+};
