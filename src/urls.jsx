05314e2fb14c39a145ec94b7068de349b9756063
ANISH0309 <mittalanish789@gmail.com>
2021-09-08 15:13:55 +0530

    fix: change client url
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- src/urls.jsx
+++ src/urls.jsx
@@ -1,5 +1,5 @@
 export default {
-  CLIENT_BASEURL: "http://localhost:3000",
+  CLIENT_BASEURL: "https://acta.vercel.app",
   SERVER_BASEURL: "http://localhost:9000",
   GOOGLE_URL: "http://localhost:9000/auth/google",
   GOOGLE_LOGOUT: "http://localhost:9000/auth/logout",
