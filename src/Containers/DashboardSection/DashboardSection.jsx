c8cd3a9c5eeba1562623662eb205040af9611b8e
ANISH0309 <mittalanish789@gmail.com>
2021-09-23 00:51:35 +0530

    feat: add search functionality in dashboard section
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- src/Containers/DashboardSection/DashboardSection.jsx
+++ src/Containers/DashboardSection/DashboardSection.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
 /* eslint-disable no-underscore-dangle */
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
@@ -11,11 +13,13 @@ import Aux from "../../hoc/Aux/Aux";
 import AddButton from "../../components/AddButton/AddButton";
 import DashCard from "../../components/DashCard/DashCard";
 import DashCardHeading from "../../components/DashCardHeading";
+import SearchIcon from "../../Assets/SearchIcon.svg";
 
 const dashboardSection = () => {
   const path = useLocation();
   const [heading, setHeading] = useState("");
   const [dashCard, setDashCard] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const token = path.search.slice(7);
@@ -54,24 +58,46 @@ const dashboardSection = () => {
     <Aux>
       <Navbar />
       <section className="container mt-2 mx-auto">
-        <div className="flex-col mx-2">
-          <div className="font-600 text-3xl sm:text-5xl">
-            {`Welcome ${heading}`}
+        <div className="flex flex-col md:flex md:flex-row justify-between">
+          <div className="flex-col mx-2 order-2 md:order-1">
+            <div className="font-600 text-3xl sm:text-5xl">
+              {`Welcome ${heading}`}
+            </div>
+            <div className="font-500 text-md sm:text-lg mt-2">
+              Here are your MOMs
+            </div>
           </div>
-          <div className="font-500 text-md sm:text-lg mt-2">
-            Here are your MOMs
+          <div className="flex h-8 justify-between border rounded-xl border-black w-56 px-2 mr-2 mb-12 md:mb-0 mt-3 md:mt-0 ml-2 md:ml-0 order-1 md:order-2">
+            <input
+              className="relative text-sm text-black py-1 px-2 w-32 sm:w-48 focus:outline-none"
+              type="text"
+              placeholder="Search..."
+              onChange={(event) => {
+                setSearchTerm(event.target.value);
+              }}
+            />
+            <img className="mr-2 w-6" src={SearchIcon} alt="search" />
           </div>
         </div>
         <div>
           <DashCardHeading />
 
-          {dashCard.map((val) => (
-            <DashCard
-              title={val.title}
-              date={moment(val.createdAt).format("MMM Do YY")}
-              key={val._id}
-            />
-          ))}
+          {dashCard
+            .filter((val) => {
+              if (searchTerm === "") {
+                return val;
+              }
+              if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return val;
+              }
+            })
+            .map((val) => (
+              <DashCard
+                title={val.title}
+                date={moment(val.createdAt).format("MMM Do YY")}
+                key={val._id}
+              />
+            ))}
         </div>
       </section>
       <AddButton />
