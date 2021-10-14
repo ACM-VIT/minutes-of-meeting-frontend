3e2aee979956a3518da618503f104f8627d57236
ANISH0309 <mittalanish789@gmail.com>
2021-10-14 23:44:23 +0530

    fix: alignment of heading from the top and no mom found logic
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- src/components/404/404.jsx
+++ src/components/404/404.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound404 = () => (
   <div className="container mx-auto mt-8">
@@ -8,11 +9,11 @@ const NotFound404 = () => (
       <div className="text-xl mt-2">
         We're sorry, this resource is not found.
       </div>
-      <a href="/dashboard">
+      <Link to="/dashboard">
         <div className="mt-2 bg-transparent hover:bg-primary text-blue-400 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded inline-block">
           Go to Dashboard
         </div>
-      </a>
+      </Link>
     </div>
   </div>
 );
