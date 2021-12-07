0977fd2d079cecb5fe5af55e508ede1c2919c2f2
ANISH0309 <mittalanish789@gmail.com>
2021-12-08 01:29:46 +0530

    feat: add loader in Dashboard section
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- src/urls.jsx
+++ src/urls.jsx
@@ -1,5 +1,8 @@
 export default {
-  SERVER_BASEURL: "https://acta-backend.vercel.app",
-  GOOGLE_URL: "https://acta-backend.vercel.app/auth/google",
-  GOOGLE_LOGOUT: "https://acta-backend.vercel.app/auth/logout",
+  // SERVER_BASEURL: "https://acta-backend.vercel.app",
+  // GOOGLE_URL: "https://acta-backend.vercel.app/auth/google",
+  // GOOGLE_LOGOUT: "https://acta-backend.vercel.app/auth/logout",
+  SERVER_BASEURL: "http://localhost:9000",
+  GOOGLE_URL: "http://localhost:9000/auth/google",
+  GOOGLE_LOGOUT: "http://localhost:9000/auth/logout",
 };
